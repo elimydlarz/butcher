8516900dcabcec3713b308e6ab1bb7379090cf40
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-03-21 14:35:07 +1100

    Added meetup venue names.

--- spec/butcher_spec.rb
+++ spec/butcher_spec.rb
@@ -22,26 +22,34 @@ describe 'butcher' do
            key: api_key_stub
          }
       ).to_return(
-        body: response_from_meetup.to_json
+        body: response_from_meetup_dot_com
       )
     end
 
     context 'when meetup.com returns a list of meetups' do
-      let(:response_from_meetup) { JSON.parse(open_events_melbourne_fixture, symbolize_names: true) }
-      let(:open_events_melbourne_fixture) { File.read('./spec/fixtures/open_events_melbourne.json') }
+      open_events_melbourne_fixture = JSON.parse(
+        File.read('./spec/fixtures/open_events_melbourne.json'),
+        symbolize_names: true
+      )
+      meetups = open_events_melbourne_fixture[:results]
 
-      it 'displays the meetup names' do
-        response_from_meetup[:results].each do |meetup|
+      let(:response_from_meetup_dot_com) { open_events_melbourne_fixture.to_json }
+
+      meetups.each do |meetup|
+        it 'displays the meetup name' do
           expect(subject.body).to include meetup[:name]
         end
-      end
 
-      it 'displays the group names' do
-        response_from_meetup[:results].each do |meetup|
+        it 'displays the meetup group name' do
           expect(subject.body).to include meetup[:group][:name]
+        end
+
+        it 'displays the meetup venue' do
+          if meetup.has_key? :venue
+            expect(subject.body).to include meetup[:venue][:name]
+          end
         end
       end
     end
   end
 end
-
